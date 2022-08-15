f387300597f6a9e62908c2da16c6b644d7100593
Matt Lukac <mattmlukac@gmail.com>
2022-08-15 11:09:28 -0700

    equations subpackage

--- product_fem/__init__.py
+++ product_fem/__init__.py
@@ -7,5 +7,3 @@ from .inverse_problems import InverseProblem
 from .loss_functionals import LossFunctional, ReducedLossFunctional
 from .solvers import Solver
 from .transforms import to_array, to_Function
-import product_fem.equations as equations
-from .eqns import HittingTimes 
---
+++ product_fem/equations/__init__.py
@@ -0,0 +1,4 @@
+from .poisson import Poisson
+from .drift_diffusion import DriftDiffusion
+from .exp_diffusion import ExpDiffusion
+from .spatial_coalescence import HittingTimes
