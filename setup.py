1c8aab5c86bc45af84e96f2aa2256b74b9fc3680
peter <petrel.harp@gmail.com>
2022-10-28 14:27:33 -0700

    update packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
-from codecs import open as codecs_open
-from setuptools import setup, find_packages
-from warnings import warn
+from setuptools import setup
 import os
-
-
-# Get the long description from the relevant file
-with codecs_open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
 
 # After exec'ing this file we have psf_version defined.
 psf_version = None  # Keep PEP8 happy.
@@ -14,33 +7,9 @@ version_file = os.path.join("product_fem", "_version.py")
 with open(version_file) as f:
     exec(f.read())
 
-setup(name='product_fem',
-      version=psf_version,
-      description=u"Solve equations on product function spaces.",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      classifiers=[],
-      keywords=['finite elements', 'population genetics'],
-      author=u"Matt Lukac",
-      author_email='mlukac@uoregon.edu',
-      url='https://github.com/mattlukac/product-space-FEM',
-      license='MIT',
-      packages=['product_fem'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'fenics',
-          'matplotlib',
-          'numpy',
-          'scipy',
-      ],
-      extras_require={
-          'dev': [],
-      },
-
-      setup_requires=[],
-      project_urls={
-          'Bug Reports': 'https://github.com/mattlukac/product-space-FEM/issues',
-          'Source': 'https://github.com/mattlukac/product-space-FEM',
-      },
+setup(
+    # The package name along with all the other metadata is specified in setup.cfg
+    # However, GitHub's dependency graph can't see the package unless we put this here.
+    name='product_fem',
+    version=psf_version,
 )
