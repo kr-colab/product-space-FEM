150b7b2e6eeb027bdc9a19e89e05b38bb80f325a
peter <petrel.harp@gmail.com>
2023-01-20 16:20:59 -0800

    simulation set-up

---
+++ simulation/compute_stats.py
@@ -0,0 +1,133 @@
+import sys, os
+import warnings
+import pyslim, tskit, msprime
+import numpy as np
+import pandas as pd
+from string import ascii_lowercase as letters
+
+import matplotlib.pyplot as plt
+
+
+if not len(sys.argv) in (2, 3):
+    print(f"""
+    Usage:
+        python {sys.argv[0]} <input>.trees [seed]
+    """)
+    sys.exit()
+
+ts_file = sys.argv[1]
+if not os.path.isfile(ts_file):
+    raise ValueError(f"File {ts_file} does not exist.")
+
+if len(sys.argv) < 3:
+    rng = np.random.default_rng()
+    seed = rng.integers(1000000)
+
+rng = np.random.default_rng(seed=seed)
+
+basename = ts_file.replace(".trees", "")
+basedir = f"{basename}_stats"
+if not os.path.exists(basedir):
+    os.makedirs(basedir)
+repname = os.path.join(basedir, f"rep{seed}")
+
+# parameters
+mut_rate = 2e-8  # mutation rate
+num_samples = 20  # number of sampled diploids
+ancestral_Ne = 1e3  # ancestral pop size
+
+print(
+        f"Reading in from {ts_file}, outputting to {basedir}"
+)
+
+warnings.simplefilter('ignore', msprime.TimeUnitsMismatchWarning)
+
+# Load tree sequence, reduce to samples today 
+ts = tskit.load(ts_file)
+_alive_nodes = np.array(
+        [n for i in pyslim.individuals_alive_at(ts, 0)
+            for n in ts.individual(i).nodes
+])
+ts = ts.simplify(_alive_nodes, keep_input_roots=True)
+
+ts = pyslim.recapitate(
+        ts,
+        ancestral_Ne=ancestral_Ne,
+        recombination_rate=1e-8,
+)
+
+ts = msprime.sim_mutations(
+        ts,
+        rate=mut_rate,
+        model=msprime.SLiMMutationModel(type=0),
+        keep=False,
+)
+
+####
+# compute statistics
+samples = rng.choice(pyslim.individuals_alive_at(ts, 0), size=num_samples)
+sample_nodes = np.array([
+    ts.individual(i).nodes
+    for i in samples
+])
+
+lll = [letters[k] for k in range(len(letters))]
+names = ["".join([a, b, c]) for a in rng.permuted(lll) for b in rng.permuted(lll) for c in rng.permuted(lll)][:len(samples)]
+
+locs = pd.DataFrame({
+    "loc" : names,
+    "x" : ts.individual_locations[samples, 0],
+    "y" : ts.individual_locations[samples, 1],
+}).set_index("loc")
+
+# heterozygosity
+locs["het"] = ts.diversity(sample_nodes, mode='site')
+
+# write out text file
+locs.to_csv(f"{repname}.stats.csv")
+
+###
+# pairwise stats
+
+pairs = pd.DataFrame(
+            np.array(
+                [[a, b] for a in locs.index for b in locs.index if a <= b]
+            ),
+            columns=["loc1", "loc2"],
+)
+pairs["dxy"] = np.nan
+
+plist = []
+nlist = list(locs.index)
+for a, b in zip(pairs["loc1"], pairs["loc2"]):
+    plist.append((nlist.index(a), nlist.index(b)))
+
+pairs["dxy"] = ts.divergence(
+        sample_sets = sample_nodes,
+        indexes = plist,
+        mode = 'site',
+)
+
+# write out text file
+pairs.to_csv(f"{repname}.pairstats.csv")
+
+# make plot of IBD
+i1 = np.array([nlist.index(i) for i in pairs['loc1']])
+i2 = np.array([nlist.index(i) for i in pairs['loc2']])
+pairs['dist'] = np.sqrt(
+    (ts.individual_locations[i1, 0] - ts.individual_locations[i2, 0])**2
+    + 
+    (ts.individual_locations[i1, 1] - ts.individual_locations[i2, 1])**2
+)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.scatter(pairs['dist'], pairs['dxy'])
+ax.set_xlabel("geographic distance")
+ax.set_ylabel("genetic distance")
+ax.set_title(repname)
+plt.tight_layout()
+
+fig.savefig(f"{repname}.ibd.png")
+
+
+print(f"All done with {basedir} =) =)")
---
+++ simulation/maps/map_utils.py
@@ -0,0 +1,174 @@
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+def floats_to_rgb(x, min=-1, max=1):
+    """
+    Translates floats in [min, max) to valid RBG integers, in [0, 255].
+    Values are clamped to min and max.
+    """
+    out = (255 * (x - min) / (max - min))
+    out[out < 0] = 0
+    out[out > 255] = 255
+    assert np.min(out) >= 0 and np.max(out) <= 255
+    return out.astype("uint8")
+
+
+def rgb_to_floats(x, min=-1, max=1):
+    """
+    The "inverse" of floats_to_rgb.
+    Note the denominator is 255, mirroring SLiM.
+    """
+    out = min + (max - min) * x.astype('float') / 255
+    return out
+
+
+def slope_layers(height):
+    """
+    Given an (n + 1, m + 1)-layer ``height``, return the (n, m, 2) layer that has the
+    x- and y-components of the slope of ``height``, as follows: if the heights surrounding
+    a square are
+    > a b
+    > c d
+    then we compute the slope there as
+    > ( (b - a)/2 + (d - c)/2, (c - a)/2 + (d - b)/2 )
+    """
+    dx = np.diff(height, axis=1)
+    dy = np.diff(height, axis=0)
+    return np.stack([
+            (dx[1:,:] + dx[:-1,:]) / 2,
+            (dy[:,1:] + dy[:,:-1]) / 2
+        ], axis=-1)
+
+
+def bump_height(nrow, ncol, width=None, center=None):
+    """
+    Return a (nrow x ncol) numpy array with values given by the bump function
+    >  exp(- 1 / (1 - r^2) ),
+    where
+    >  r = sqrt( (x/width[0])^2 + (y/width[1])^2 )
+    for -width[0] < x < width[0] and -width[1] , y < width[1].
+    """
+    if center is None:
+        center = np.array([(nrow - 1) / 2, (ncol - 1) / 2])
+    if width is None:
+        width = center
+    x = np.repeat([np.arange(nrow) - center[0]], ncol, axis=1)
+    y = np.repeat([np.arange(ncol) - center[1]], nrow, axis=0).flatten()
+    z = np.maximum(0.05, 1 - ((x/width[0]) ** 2 + (y/width[1]) ** 2))
+    out = np.exp(- 1 / z)
+    out[out < 0] = 0.0
+    out.shape = (nrow, ncol)
+    return(out)
+
+
+def mountain_height(nrow, ncol, slope=None, center=None):
+    """
+    Return a (nrow x ncol) numpy array that has value 1.0 at ``center``
+    and declines linearly with ``slope`` to zero.
+    """
+    if center is None:
+        center = np.array([(nrow - 1) / 2, (ncol - 1) / 2])
+    if slope is None:
+        # put 0.0 at the further edge of the smaller dimension
+        slope = 1.0 / min(max(ncol - center[0], center[0]),
+                          max(nrow - center[1], center[1]))
+    x = np.repeat([np.arange(nrow) - center[0]], ncol, axis=1)
+    y = np.repeat([np.arange(ncol) - center[1]], nrow, axis=0).flatten()
+    dist = np.sqrt(x ** 2 + y ** 2)
+    out = 1.0 - dist * slope
+    out[out < 0] = 0.0
+    out.shape = (nrow, ncol)
+    return(out)
+
+
+def make_slope_rgb(nrow, ncol, height_fn, **kwargs):
+    if 'center' in kwargs:
+        center = kwargs['center']
+        kwargs['center'] = [center[0] * (1 + 1/nrow), center[1] * (1 + 1/ncol)]
+    height = height_fn(
+            nrow + 1,
+            ncol + 1,
+            **kwargs)
+    slope = slope_layers(height)
+    out = np.concatenate([
+            floats_to_rgb(slope / np.max(np.abs(slope)), min=-1, max=1),
+            np.full((nrow, ncol, 1), 0, dtype='uint8'),
+            np.full((nrow, ncol, 1), 255, dtype='uint8')
+        ], axis=-1)
+    return out.astype("uint8")
+
+
+def make_sigma_rgb(nrow, ncol, height_fn, **kwargs):
+    # uses same sigma in x and y direction; no correlation
+    # do it by averaging the +1 grid to agree with slope
+    if 'center' in kwargs:
+        center = kwargs['center']
+        kwargs['center'] = [center[0] * (1 + 1/nrow), center[1] * (1 + 1/ncol)]
+    height = height_fn(
+            nrow + 1,
+            ncol + 1,
+            **kwargs)
+    sigma = floats_to_rgb((height[:-1,:-1]
+                           + height[1:,:-1]
+                           + height[:-1,1:]
+                           + height[1:,1:])[:,:,np.newaxis] / 4,
+                          min=-1, max=1)
+    zero = floats_to_rgb(np.full((nrow, ncol, 1), 0), min=-1, max=1)
+    out = np.concatenate([
+            sigma,
+            sigma,
+            zero,
+            np.full((nrow, ncol, 1), 255, dtype='uint8')
+        ], axis=-1)
+    return out.astype("uint8")
+
+
+def mountain_slope(nrow, ncol, slope=None, center=None):
+    """
+    Make a (nrow, ncol, 4) RGBA array with layers corresponding to
+    (downslope bias x, downslope bias y, 0, 255)
+    on a "stratovolcano" (linear cone).
+    """
+    if center is None:
+        center = np.array([nrow / 2, ncol / 2])
+    return make_slope_rgb(
+            nrow, ncol, mountain_height,
+            slope=slope, center=center)
+
+def mountain_sigma(nrow, ncol, slope=None, center=None):
+    """
+    Make a (nrow, ncol, 4) RGBA array with layers corresponding to
+    (sigma x, sigma y, 0, 255)
+    on a "stratovolcano" (linear cone).
+    """
+    if center is None:
+        center = np.array([nrow / 2, ncol / 2])
+    return make_sigma_rgb(
+            nrow, ncol, mountain_height,
+            slope=slope, center=center)
+
+def butte_slope(nrow, ncol, width=None, center=None):
+    """
+    Make a (nrow, ncol, 4) RGBA array with layers corresponding to
+    (downslope bias x, downslope bias y, 0, 255)
+    on a "butte" (a bump function).
+    """
+    if center is None:
+        center = np.array([nrow / 2, ncol / 2])
+    return make_slope_rgb(
+            nrow, ncol, bump_height,
+            width=width, center=center)
+
+def butte_sigma(nrow, ncol, width=None, center=None):
+    """
+    Make a (nrow, ncol, 4) RGBA array with layers corresponding to
+    (sigma x, sigma y, 0, 255)
+    on a "butte" (a bump function).
+    """
+    if center is None:
+        center = np.array([nrow / 2, ncol / 2])
+    return make_sigma_rgb(
+            nrow, ncol, bump_height,
+            width=width, center=center)
