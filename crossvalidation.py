6d9a230f9d2ed8c756ed4503c7bfc8206e1ff5d3
silastittes <silas.tittes@gmail.com>
2023-03-31 19:20:20 +0000

    added argument parsing, declobbered grid search output

--- crossvalidation.py
+++ crossvalidation.py
@@ -2,54 +2,91 @@ import os, sys, pickle
 import numpy as np
 import pandas as pd
 import json
+import argparse
 from hyperopt import hp, fmin, tpe, space_eval
 
 import product_fem as pf
 import fenics
 import inference
 
-usage = f"""
-Usage:
-    {sys.argv[0]} (json file with parameters in)
-and the json file should include:
-    "spatial_data", "genetic_data" : file paths
-    "mesh" : either an XML file or a [width, height] of integers
-    "folds": number of folds
-    "regularization": {{ "l2": [a, b], "smoothing": [c, d] }}
-        where the second value for each is the *ratio* of regularization
-        strengths for the two controls (ellipse and vector)
-    "boundary": {{ "epsilon": x, "eps0": y, "eps1": z }}
-    "method": method for optimizing (default: BFGS)
-    "options": additional options to the optimizer
-"""
 
-if len(sys.argv) != 2:
-    print(usage)
-    sys.exit()
 
-def objective(args):
-    l2_0, l2_1, smooth_0, smooth_1 = args
+def parse_args(args):
+    # Instantiate the parser
+    parser = argparse.ArgumentParser(
+        description="""regularization tuning  of Product Space FEM 
+            using cross validation
+        """
+    )
+
+    parser.add_argument(
+        "--json",
+        type=str,
+        required=True,
+        help="""Path to JSON file with.
+        should include:
+            "spatial_data", "genetic_data" : file paths
+            "mesh" : either an XML file or a [width, height] of integers
+            "folds": number of folds
+            "regularization": {{ "l2": [a, b], "smoothing": [c, d] }}
+                where the second value for each is the *ratio* of regularization
+                strengths for the two controls (ellipse and vector)
+            "boundary": {{ "epsilon": x, "eps0": y, "eps1": z }}
+            "method": method for optimizing (default: BFGS)
+            "options": additional options to the optimizer
+        """,
+    )
+
+    parser.add_argument(
+        "-H",
+        "--use_hyperopt",
+        action="store_true",
+        help="""Switch to tune parameters using hyperopt instead of grid search.
+    """,
+    )
+
+    parser.add_argument(
+        "-e",
+        "--max_evals",
+        type=int,
+        default=100,
+        help="""Maximum number of evaluations for hyperopt.""",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--max_iter",
+        type=int,
+        default=100,
+        help="""Maximum number of iterations for model optimization.""",
+    )
+
+    return parser.parse_args()
+
+
+def objective(tuning_params = None):
     # defaults
     params = {
         "method": "BFGS",
         "options": {
             'gtol': 1e-8,
             'xrtol': 1e-8,
-            'maxiter': 100
+            'maxiter': args.max_iter,
         },
         "boundary": None,
     }
 
-    paramsfile = sys.argv[1]
+    paramsfile = args.json
     outdir = os.path.dirname(paramsfile)
     with open(paramsfile, 'r') as f:
         file_params = json.load(f)
 
     params.update(file_params)
     
-    #can't find files if I don't do this
-    outdir = '/'.join(outdir.split("/")[:-1]) 
-    results_file = os.path.join(outdir, 'results.pkl')
+    if args.use_hyperopt:
+        results_file = os.path.join(outdir, 'results_hyperopt.pkl')
+    else:
+        results_file = os.path.join(outdir, 'results.pkl')
 
     # load spatial and genetic data
     spatial_data = pd.read_csv(os.path.join(outdir, params['spatial_data'])).rename(
@@ -83,8 +120,11 @@ def objective(args):
     W = pf.ProductFunctionSpace(V)
 
     errs = []
-    params['regularization']['l2'] = [l2_0, l2_1]
-    params['regularization']['smoothing'] = [smooth_0, smooth_1]
+    if args.use_hyperopt:
+        l2_0, l2_1, smooth_0, smooth_1 = tuning_params
+        params['regularization']['l2'] = [l2_0, l2_1]
+        params['regularization']['smoothing'] = [smooth_0, smooth_1]
+
     results = {'params': params}
     for fold, (train, test) in enumerate(data.split(k=params["folds"], include_between=True)):
         print(f"Doing fold {fold} with {params['method']}...")
@@ -127,6 +167,7 @@ def objective(args):
         errs.append(test_error)
         print(f"Done: test error {test_error}")
 
+
     print(f'saving file {results_file}')
     with open(results_file, 'wb') as file:
         pickle.dump(results, file, protocol=pickle.HIGHEST_PROTOCOL)
@@ -135,10 +176,14 @@ def objective(args):
 
 
 if __name__ == "__main__":
-    # define a search space
-    space = [hp.lognormal('l2_0', 0, 1), hp.lognormal('l2_1', 0, 2), hp.lognormal('smooth_0', 0, 2), hp.lognormal('smooth_1', 0, 2)] 
 
-    # minimize the objective over the space
-    best = fmin(objective, space, algo=tpe.suggest, max_evals=20)
-
-    print(best)
+    args = parse_args(sys.argv[1:])
+    
+    if args.use_hyperopt:
+        # define a search space
+        space = [hp.lognormal('l2_0', 0, 2), hp.lognormal('l2_1', 0, 2), hp.lognormal('smooth_0', 0, 2), hp.lognormal('smooth_1', 0, 2)] 
+        # minimize the objective over the space
+        best = fmin(objective, space, algo=tpe.suggest, max_evals=args.max_evals)
+        print(best)
+    else:
+        objective()
