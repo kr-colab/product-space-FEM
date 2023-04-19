a73ca7c4fd6fdceea6ea648df29275599d346612
peter <petrel.harp@gmail.com>
2023-04-19 10:20:15 -0700

    skinny maps

--- simulation/maps/make_maps.py
+++ simulation/maps/make_maps.py
@@ -11,68 +11,70 @@ from map_utils import *
 # format for habitat (monochrome) is:
 #  K = carrying capacity
 
-h, w = (20 * 20, 20 * 30)
+for (h, w, name) in [
+        (20 * 20, 20 * 30, ""),
+        (20 * 2, 20 * 30, "skinny_"),
+    ]:
 
-# DEFAULT (constant) MAPS
-# all ones
-one = np.full((h, w, 4), 1.0)
-rgb = floats_to_rgb(one, min=0, max=1)
-im = Image.fromarray(rgb)
-im.save("one.png", mode="L")
-im = Image.fromarray(rgb[:,:,:2])
-im.save("one_bw.png", mode="L")
+    # DEFAULT (constant) MAPS
+    # all ones
+    one = np.full((h, w, 4), 1.0)
+    rgb = floats_to_rgb(one, min=0, max=1)
+    im = Image.fromarray(rgb)
+    im.save(f"{name}one.png", mode="L")
+    im = Image.fromarray(rgb[:,:,:2])
+    im.save(f"{name}one_bw.png", mode="L")
 
-# all ones, b&w
+    # all ones, b&w
 
-# the identity map for sigma
-ident = np.full((h, w, 4), 1.0)
-ident[:,:,2] = 0.0
-rgb = floats_to_rgb(ident, min=-1, max=1)
-im = Image.fromarray(rgb)
-im.save("identity.png", mode="L")
+    # the identity map for sigma
+    ident = np.full((h, w, 4), 1.0)
+    ident[:,:,2] = 0.0
+    rgb = floats_to_rgb(ident, min=-1, max=1)
+    im = Image.fromarray(rgb)
+    im.save(f"{name}identity.png", mode="L")
 
-# MOUNTAIN: downhill on a stratovolcano
-rgb = mountain_slope(h, w)
-im = Image.fromarray(rgb)
-im.save("mountain_bias.png")
+    # MOUNTAIN: downhill on a stratovolcano
+    rgb = mountain_slope(h, w)
+    im = Image.fromarray(rgb)
+    im.save(f"{name}mountain_bias.png")
 
-rgb = mountain_sigma(h, w)
-im = Image.fromarray(rgb)
-im.save("mountain_sigma.png")
+    rgb = mountain_sigma(h, w)
+    im = Image.fromarray(rgb)
+    im.save(f"{name}mountain_sigma.png")
 
-height = floats_to_rgb(mountain_height(h, w), min=0, max=1)
-im = Image.fromarray(height)
-im.save("mountain_height.png", mode="L")
+    height = floats_to_rgb(mountain_height(h, w), min=0, max=1)
+    im = Image.fromarray(height)
+    im.save(f"{name}mountain_height.png", mode="L")
 
-# DOWNHILL: downhill on the gaussian density
-rgb = gaussian_slope(h, w)
-im = Image.fromarray(rgb)
-im.save("gaussian_bias.png")
+    # DOWNHILL: downhill on the gaussian density
+    rgb = gaussian_slope(h, w)
+    im = Image.fromarray(rgb)
+    im.save(f"{name}gaussian_bias.png")
 
-# SADDLE: saddlewise on the gaussian density
-rgb = saddle_slope(h, w)
-im = Image.fromarray(rgb)
-im.save("saddle_bias.png")
+    # SADDLE: saddlewise on the gaussian density
+    rgb = saddle_slope(h, w)
+    im = Image.fromarray(rgb)
+    im.save(f"{name}saddle_bias.png")
 
-x = saddle_height(h, w)
-x -= np.min(x)
-x /= np.max(x)
-print("saddle", np.min(x), np.max(x))
-height = floats_to_rgb(x, min=0, max=1)
-im = Image.fromarray(height)
-im.save("saddle_height.png", mode="L")
+    x = saddle_height(h, w)
+    x -= np.min(x)
+    x /= np.max(x)
+    height = floats_to_rgb(x, min=0, max=1)
+    im = Image.fromarray(height)
+    im.save(f"{name}saddle_height.png", mode="L")
 
-# BUTTE: downhill on a bump function
-rgb = butte_slope(h, w)
-im = Image.fromarray(rgb)
-im.save("butte_bias.png")
+    # BUTTE: downhill on a bump function
+    rgb = butte_slope(h, w)
+    im = Image.fromarray(rgb)
+    im.save(f"{name}butte_bias.png")
 
-rgb = butte_sigma(h, w)
-im = Image.fromarray(rgb)
-im.save("butte_sigma.png")
+    rgb = butte_sigma(h, w)
+    im = Image.fromarray(rgb)
+    im.save(f"{name}butte_sigma.png")
 
-x = bump_height(h, w)
-x /= np.max(x)
-height = floats_to_rgb(x, min=0, max=1)
-im = Image.fromarray(height)
-im.save("butte_height.png", mode="L")
+    x = bump_height(h, w)
+    x /= np.max(x)
+    height = floats_to_rgb(x, min=0, max=1)
+    im = Image.fromarray(height)
+    im.save(f"{name}butte_height.png", mode="L")
