d48c754e561db0353e90f1efcad1fda1b1ddf159
peter <petrel.harp@gmail.com>
2023-08-09 13:58:35 -0700

    comment out mshr so it runs

--- product_fem/__init__.py
+++ product_fem/__init__.py
@@ -6,8 +6,8 @@ from .function_spaces import ProductFunctionSpace
 from .functions import ProductFunction, Control, SpatialData
 from .inverse_problems import InverseProblem, taylor_test
 from .loss_functionals import LossFunctional, ReducedLossFunctional
-from .meshes import polygon_geom, polygon_mesh, convex_hull_geom, convex_hull_mesh, rectangle_geom, rectangle_mesh, ellipse_geom, ellipse_mesh, square_geom, square_mesh, unit_square_geom, unit_square_mesh, disc_geom, disc_mesh, unit_disc_geom, unit_disc_mesh
-from mshr import generate_mesh
+# from .meshes import polygon_geom, polygon_mesh, convex_hull_geom, convex_hull_mesh, rectangle_geom, rectangle_mesh, ellipse_geom, ellipse_mesh, square_geom, square_mesh, unit_square_geom, unit_square_mesh, disc_geom, disc_mesh, unit_disc_geom, unit_disc_mesh
+# from mshr import generate_mesh
 from .plotting import plot, animate_control
 from .solvers import Solver
 from .transforms import to_array, to_Function
